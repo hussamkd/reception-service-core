745f956276df0035a0db8dc0f72a12fabefc5624
Hussam Abdulrahman <hussamkd99@gmail.com>
2023-03-16 15:04:30 +0300

    Initial commit(First Push)

---
+++ src/main/java/com/ats/receptionservicecore/ReceptionServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ats.receptionservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionServiceCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ats/receptionservicecore/ReceptionServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.receptionservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptionServiceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
